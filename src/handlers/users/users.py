6a6178802167ea18a94b8f90ff47be68cde62ab0
Windows 10 <adhambekhusinboyev@gmail.com>
2025-07-09 12:50:17 +0500

    first commit

--- src/handlers/users/users.py
+++ src/handlers/users/users.py
@@ -1,6 +1,10 @@
-from aiogram import Router
+from aiogram import Router, F
+from aiogram.enums import ChatType
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+from config import bot
+from src.keyboards.keyboard_func import CheckData
 
 user_router = Router()
 
@@ -8,3 +12,13 @@ user_router = Router()
 @user_router.message(CommandStart())
 async def start_cmd1(message: Message):
     await message.answer("Botimizga xush kelibsiz, kayp qilish uchun admin bilan bog'laning!\n\n@adkhambek_4", parse_mode="html")
+
+
+@user_router.message(F.text == "📊 Ball yetadigan yo'nalishlar", F.chat.type == ChatType.PRIVATE)
+async def enter_direction(message: Message):
+    check_status, channels = await CheckData.check_member(bot, message.from_user.id)
+    if check_status:
+        await message.answer("<b>kayp qilish uchun admin bilan bog'laning</b>", parse_mode="html")
+    else:
+        await message.answer("❗ Iltimos, quyidagi kanallarga a’zo bo‘ling:",
+                             reply_markup=await CheckData.channels_btn(channels))
